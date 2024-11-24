6ca388a4855eeb49ccfc0fa9e7abcbe043fa7dbd
Jefferson Oliveira <jefferoidtex7@gmail.com>
2024-11-23 23:23:55 -0300

    feat: include container

--- app/plugins/blocks-studio-stg/tailwind.config.js
+++ app/plugins/blocks-studio-stg/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     content: ["./src/**/*.{html,js,scss,css,php}"],
     theme: {
       extend: {},
+      container: {
+        center: true,
+      },
+      screens: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1400px',
+      },
     },
     plugins: [],
   }
